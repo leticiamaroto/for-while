2d65d8e6df9caeeffe7fef0efbed3bc3bf093d1d
letícia <leticiamarotods01@outlook.com>
2022-01-25 16:55:00 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+//for
+
+for(let i = 1; i <= 10; i++) {
+    console.log("for", i);
+}
+
+//while
+
+let contador = 10;
+while(contador > 0){
+    console.log("while", contador)
+    contador--
+}
+
+//impares
+
+for(let i = 1; i <= 100; i += 2){
+    console.log("impar", i);
+}
+
+//pares
+
+for(let i = 0; i <= 100; i += 2){
+    console.log("par", i);
+}
